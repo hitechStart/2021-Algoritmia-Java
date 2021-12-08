87bc56d627d7b541b81dace7582dd11443a4cc73
hitechStart <mdlprofesional@gmail.com>
2021-12-08 16:48:09 -0300

    Javadoc modificado

--- 1-SERVILOGIST/src/main/java/trainning/servilogist/Modo.java
+++ 1-SERVILOGIST/src/main/java/trainning/servilogist/Modo.java
@@ -8,10 +8,10 @@ public interface Modo {
 
     /**
      * El metodo mando tiene por objetivo explicar la forma en que se maneja el vehiculo
-     * @guiaDeMando Tipo de mando. Ejemplo: un Camion utiliza un volante
-     * @TipoDeacelerador Forma de acelerar el vehiculo. Ejemplo un acelerador de
+     * @param guiaDeMando Tipo de mando. Ejemplo: un Camion utiliza un volante
+     * @param TipoDeacelerador Forma de acelerar el vehiculo. Ejemplo un acelerador de
      * pie mecanico
-     * @FormaDeFrenado Forma de freno. Ejemplo de un camion es el freno de pie
+     * @param FormaDeFrenado Forma de freno. Ejemplo de un camion es el freno de pie
      * mecanico.
      */
     public String mando(String guiaDeMando,String TipoDeacelerador,String FormaDeFrenado);
--- 1-SERVILOGIST/target/site/apidocs/javadoc.bat
+++
@@ -1,1 +0,0 @@
-cmd.exe /X /C "D:\jdk-11.0.12\bin\javadoc.exe @options @packages @argfile"
